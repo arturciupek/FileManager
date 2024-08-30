e01ee0afdafa795264226f31ce36c86f7eac09c1
arturciupek <arturciupek18@gmail.com>
2024-08-30 16:15:17 +0200

    Stworzenie ciała metody FileManager

--- src/main/java/org/example/FileManager.java
+++ src/main/java/org/example/FileManager.java
@@ -1,5 +1,15 @@
 package org.example;
 
+import java.io.File;
+import java.util.Objects;
+import java.util.stream.Stream;
+
 public class FileManager {
-    public void listFile () {
-    }}
+    public void listFileInDirectory(String directory) {
+        Stream.of(Objects.requireNonNull(new File(directory).listFiles()))
+                .filter(File::isFile)
+                .map(File::getName)
+                .forEach(System.out::println);
+
+    }
+}
