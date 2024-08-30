e7a63986f729bceef3af0ee7eb225edb86f11a2a
arturciupek <arturciupek18@gmail.com>
2024-08-30 16:45:13 +0200

    Dodanie kolejnej metody w klasie FileManager

--- src/main/java/org/example/FileManager.java
+++ src/main/java/org/example/FileManager.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import java.io.File;
+import java.util.Comparator;
 import java.util.Objects;
 import java.util.stream.Stream;
 
@@ -11,5 +12,15 @@ public class FileManager {
                 .map(File::getName)
                 .forEach(System.out::println);
 
+    }
+    public void sortFileInDirectory(String directory) {
+        Stream.of(Objects.requireNonNull(new File(directory).listFiles()))
+                .filter(File::isFile)
+                .sorted(Comparator.comparingLong(File::length))
+                .forEach(file -> {
+                    System.out.println(file.getName());
+                    System.out.println(file.length());
+                    System.out.println();
+                });
     }
 }
